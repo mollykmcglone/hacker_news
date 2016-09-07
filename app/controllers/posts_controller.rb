9df56e9065961b665daebc2831abfbf1ad929f58
James Turello and Molly McGlone <molly@>
2016-09-07 14:55:38 -0700

    add show view and action to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,10 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :link, :votes)
