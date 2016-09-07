d219711366c73cf9b6256884acea17bde4ec218f
James Turello and Molly McGlone <james+molly@>
2016-09-07 10:14:59 -0700

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.votes = 0
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :link, :votes)
+  end
+
+end
